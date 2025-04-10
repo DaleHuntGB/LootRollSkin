405a4e29dc6dbb2ca8b326ed63e317fa49ebbe2e
Dale Hunt <dalehunt@gmail.com>
2025-04-10 08:55:17 +0100

    Extra arg to help with toggling the settings.

--- Commands.lua
+++ Commands.lua
@@ -9,7 +9,7 @@ function LRS:SlashCommand(msg)
 	end
 	if args[1] == "anchor" or args[1] == "a" then
 		self:ToggleAnchor()
-	elseif args[1] == "settings" or args[1] == "s" then
+	elseif args[1] == "settings" or args[1] == "s" or args[1] == nil then
 		self:ToggleSettings()
 	end
 end
